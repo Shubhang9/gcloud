4ade49d2f1e569d20cc78efe4d90bde38908ee5b
Mine <Mine@DESKTOP-T7NU3TN>
2018-06-10 17:42:22 +0530

    step 14 step 7 fro spring mvc - Autowired UserValidationService.java(using @Service) in LoginController.java(using @Autowired)

--- first-web-application/src/main/java/com/in28Minutes/login/LoginController.java
+++ first-web-application/src/main/java/com/in28Minutes/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.in28Minutes.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,9 @@ import com.in28Minutes.login.UserValidationService;
 
 @Controller
 public class LoginController {
-	private UserValidationService service=new UserValidationService();
+	
+	@Autowired
+	private UserValidationService service;
 	@RequestMapping(value="/login",method=RequestMethod.GET)
 	public String showLoginPage() {
 		return "login";
--- first-web-application/src/main/java/com/in28Minutes/login/UserValidationService.java
+++ first-web-application/src/main/java/com/in28Minutes/login/UserValidationService.java
@@ -1,5 +1,8 @@
 package com.in28Minutes.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserValidationService {
 	public boolean isUserValid(String user,String password) {
 		if(user.equals("Shubhang")&&password.equals("password"))
